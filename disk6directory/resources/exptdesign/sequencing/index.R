2875eb8c05ec31104cd44da580ba3919e55f3648
arfesta <arfesta@ncsu.edu>
2018-04-08 11:37:50 -0400

    update lib file names

--- disk6directory/resources/exptdesign/sequencing/index.R
+++ disk6directory/resources/exptdesign/sequencing/index.R
@@ -1,5 +1,5 @@
 #####
-setwd("/media/disk6/ARF/RNASEQ/trimmedfiltreads")
+setwd("/media/disk6/ARF/RNASEQ/trimmedfiltreads/86k")
  the.call <-  paste('cd EW; rm all_indicies.txt; while read file; do gzip -cd $file |head -100 | grep "^@D00555" | cut -d : -f 10 | sort | uniq -c | sort -nr  >> all_indicies.txt; done < all.ew.reps',sep="")
 system(the.call,ignore.stderr = T)
 ew.index.table <- read.table("./EW/all_indicies.txt")
@@ -7,7 +7,7 @@ ew.index.table <- table(ew.index.table[,2])
 true.EW.index <- names(which(ew.index.table == 12))
 
 
-the.call <-  paste('cd LGEP; rm all_indicies.txt; while read file; do gzip -cd $file |head -100 | grep "^@HISEQ" | cut -d : -f 10 | sort | uniq -c | sort -nr  >> all_indicies.txt; done < all.lgep.reps',sep="")
+the.call <-  paste0('cd LGEP; rm all_indicies.txt; while read file; do gzip -cd $file |head -200 | grep "^@HISEQ" | cut -d : -f 10 | sort | uniq -c | sort -nr  >> all_indicies.txt; done < all.lgep.reps')
 system(the.call,ignore.stderr = T)
 
 lgep.index.table <- read.table("./LGEP/all_indicies.txt")
@@ -24,47 +24,47 @@ table(c(true.EW.index,true.LGEP.index))
 identical(true.EW.index,true.LGEP.index)
 #TRUE
 #
-
+all.ew.reps <- read.table("./EW/all.ew.reps")
 #We have the true barcodes for each batch now lets assign to samples
-i = 1
-ew.index.table <- rep(NA,288)
-while(anyNA(ew.index.table)){
- the.call <-  paste('cd EW; rm all_indicies.txt; while read file; do gzip -cd $file |head -1000 | grep "^@D00555" | sed "',as.character(i +1),'q;d" | cut -d : -f 10 | sort | uniq -c | sort -nr  >> all_indicies.txt; done < all.ew.reps',sep="")
-system(the.call,ignore.stderr = T)
-ew.index <- read.table("./EW/all_indicies.txt")
-correct <- which(ew.index[,2] %in% true.EW.index)
-ew.index.table[correct] <- as.character(ew.index[correct,2])
-i <- i + 1
+#ew.index.table <- rep(NA,288)
+ew.indicies <- vector()
+for(each.file in 1:nrow(all.ew.reps) ) {
+  file <- as.character(all.ew.reps$V1[each.file])
+
+  the.call <- paste0('gzip -cd ./EW/', file,' | head -10000 | grep "^@D00555" | cut -d : -f 10 > ../out.txt')
+  system(the.call)
+  index.count <- table(read.table(file = "/media/disk6/ARF/RNASEQ/trimmedfiltreads/out.txt")[,1])
+index.name <- names(index.count)[which(names(index.count) %in% true.EW.index)]
+ew.indicies <- c(ew.indicies,index.name)
 }
-table(ew.index.table)
+ 
+table(ew.indicies)
 #ACAGTG ACTGAT ACTTGA AGTCAA AGTTCC ATCACG ATGTCA ATTCCT CAGATC CCGTCC CGATGT CGTACG CTTGTA GAGTGG GATCAG GCCAAT GGCTAC GTCCGC 
 #    12     12     12     12     12     12     12     12     12     12     12     12     12     12     12     12     12     12 
 #GTGAAA GTGGCC GTTTCG TAGCTT TGACCA TTAGGC 
 #    12     12     12     12     12     12 
 #    
+all.lgep.reps <- read.table("./LGEP/all.lgep.reps")
+lgep.indicies <- vector()
+for(each.file in 1:nrow(all.lgep.reps) ) {
+  file <- as.character(all.lgep.reps$V1[each.file])
 
-i=1
-lgep.index.table <-  rep(NA,432)
-while(anyNA(lgep.index.table) ){
-  the.call <-  paste('cd ./LGEP; rm all_indicies.txt; while read file; do gzip -cd $file |head -1000 | grep "^@HISEQ" | sed "',as.character(i +1),'q;d" |  cut -d : -f 10 | sort | uniq -c | sort -nr  >> all_indicies.txt; done < all.lgep.reps',sep="")
-system(the.call,ignore.stderr = T)
-
-lgep.index <- read.table("./LGEP/all_indicies.txt")
-correct <- which(lgep.index[,2] %in% true.LGEP.index)
-lgep.index.table[correct] <- as.character(lgep.index[correct,2])
-i <- i+1
+  the.call <- paste0('gzip -cd ./LGEP/', file,' | head -10000 | grep "^@HISEQ" | cut -d : -f 10 > ../out.txt')
+  system(the.call)
+  index.count <- table(read.table(file = "/media/disk6/ARF/RNASEQ/trimmedfiltreads/out.txt")[,1])
+index.name <- names(index.count)[which(names(index.count) %in% true.LGEP.index)]
+lgep.indicies <- c(lgep.indicies,index.name)
 }
-table(lgep.index.table)
+ 
+table(lgep.indicies)
 #ACAGTG ACTGAT ACTTGA AGTCAA AGTTCC ATCACG ATGTCA ATTCCT CAGATC CCGTCC CGATGT CGTACG CTTGTA GAGTGG GATCAG GCCAAT GGCTAC GTCCGC 
 #    18     18     18     18     18     18     18     18     18     18     18     18     18     18     18     18     18     18 
 #GTGAAA GTGGCC GTTTCG TAGCTT TGACCA TTAGGC 
 #    18     18     18     18     18     18 
 
 #Output index and save file#####
-ew.filenames <- read.table("./EW/all.ew.reps")
-lgep.filenames <- read.table("./LGEP/all.lgep.reps")
-index=c(ew.index.table,lgep.index.table)
-file_name=c(as.character(ew.filenames[,1]),as.character(lgep.filenames[,1]))
+index=c(ew.indicies,lgep.indicies)
+file_name=c(as.character(all.ew.reps[,1]),as.character(all.lgep.reps[,1]))
 tech.rep.index <- cbind("index"=index,"file_name"=file_name)
-save(tech.rep.index,file = "../resources/exptdesign/index.RData",compress=T)
+save(tech.rep.index,file = "/media/disk6/ARF/RNASEQ/Breeding-Value-Prediction/disk6directory/resources/exptdesign/sequencing/index.RData",compress=T)
 
